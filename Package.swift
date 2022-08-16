58b8e767b4449dc0941dfa16018f9abde25fa0d0
이원주 <wjleedev@aboutpet.co.kr>
2022-08-16 10:57:49 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -5,23 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "MBCircularProgressBar",
-    platforms: [
-        .iOS(.v13),
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "MBCircularProgressBar",
             targets: ["MBCircularProgressBar"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-          name: "MBCircularProgressBar",
-          path: "Sources/MBCircularProgressBar",
-          publicHeadersPath: ".",
-          cSettings: [
-            .headerSearchPath("../../"),
-          ]
-        ),
+            name: "MBCircularProgressBar",
+            dependencies: []),
+        .testTarget(
+            name: "MBCircularProgressBarTests",
+            dependencies: ["MBCircularProgressBar"]),
     ]
 )
--- Tests/MBCircularProgressBarTests/MBCircularProgressBarTests.swift
+++ Tests/MBCircularProgressBarTests/MBCircularProgressBarTests.swift
@@ -6,6 +6,7 @@ final class MBCircularProgressBarTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MBCircularProgressBar().text, "Hello, World!")
+        
+        XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 }
